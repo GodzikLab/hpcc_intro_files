23389d7cd9d8fce44e2f28e8ae5a770202bab81c
Chuck Forsyth <forsythc@ucr.edu>
2018-06-15 12:41:52 -0700

    house keeping

--- submit_slurm_job.sh
+++ submit_slurm_job.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -l
 
-# Uncomment the line below for the slurm script you wish to run.
+# Uncomment the line or lines below for the slurm script(s) you wish to run.
 
 
-sbatch slurm_script_full_sbatch_options.sh
-sbatch slurm_script_gpu.sh
-sbatch slurm_script_highmem.sh
-sbatch slurm_script_mpi.sh
-sbatch slurm_script_multinode.sh
-sbatch slurm_script_pass_argument.sh "example argument"
+#sbatch slurm_script_full_sbatch_options.sh
+#sbatch slurm_script_gpu.sh
+#sbatch slurm_script_highmem.sh
+#sbatch slurm_script_mpi.sh
+#sbatch slurm_script_multinode.sh
+#sbatch slurm_script_pass_argument.sh "example argument"
 sbatch slurm_script_simple.sh
-sbatch slurm_script_time_report.sh
+#sbatch slurm_script_time_report.sh
 
 
